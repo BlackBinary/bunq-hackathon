8f9bcbd79f59af3117a78d16b5f792b724179035
Daen Rebel <daenrebel035@gmail.com>
2019-03-23 01:06:47 +0100

    Added receipt route and GCloud api

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,85 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
+    <div class="camera-modal">
+        <video ref="video" class="camera-stream"/>
+        <div class="camera-modal-container">
+            <span @click="capture"
+                  class="take-picture-button take-picture-button mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+              <i class="material-icons">camera</i>
+            </span>
         </div>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                mediaStream: null
+            }
+        },
         mounted() {
-            console.log('Component mounted.')
+            navigator.mediaDevices.getUserMedia({video: true})
+                .then(mediaStream => {
+                    this.mediaStream = mediaStream
+                    this.$refs.video.srcObject = mediaStream
+                    this.$refs.video.play()
+                })
+                .catch(error => console.error('getUserMedia() error:', error))
+        },
+        methods: {
+            capture() {
+                const mediaStreamTrack = this.mediaStream.getVideoTracks()[0]
+                const imageCapture = new window.ImageCapture(mediaStreamTrack)
+                return imageCapture.takePhoto().then(blob => {
+                    const data = new FormData();
+                    data.append('receipt', blob, 'receiptImage');
+                    console.log(data);
+                    axios.post('/api/receipt', data, {
+                        headers: {
+                            'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                        },
+                        timeout: 30000,
+                    }).then((data) => {
+                        console.log(data);
+                    });
+                });
+            }
+        },
+        destroyed() {
+            const tracks = this.mediaStream.getTracks()
+            tracks.map(track => track.stop())
         }
     }
 </script>
+<style scoped>
+    .camera-modal {
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        position: absolute;
+        background-color: white;
+        z-index: 10;
+    }
+
+    .camera-stream {
+        width: 100%;
+        max-height: 100%;
+    }
+
+    .camera-modal-container {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        align-items: center;
+        margin-bottom: 24px;
+    }
+
+    .take-picture-button {
+        position: fixed;
+        right: 24px;
+        bottom: 90px;
+        z-index: 5;
+        display: flex;
+    }
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,4 @@
 
 @section('content')
     <example-component></example-component>
-
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
-
-                    <div class="card-body">
-
-
-
-                        You are logged in!
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
